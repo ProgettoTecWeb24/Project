e78ca2ba796f4bf5746828013dcbc1955f198603
Eddy <149050257+EddyPinarello@users.noreply.github.com>
2024-12-03 17:26:57 +0100

    Menu mobile (#16)

---
+++ src/JS/script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    const navbarMenu = document.getElementById("navbar-menu");
+    const toggleIcon = document.getElementById("menu-toggle").querySelector("img");
+
+    navbarMenu.classList.toggle("active");
+
+    
+    if (navbarMenu.classList.contains("active")) {
+        toggleIcon.src = "../assets/close.png"; 
+    } else {
+        toggleIcon.src = "../assets/menu.png"; 
+    }
+}
