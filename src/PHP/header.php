6b00a4a80b57a8005b2105523812086d9b872c10
Blueerr <salvogiova01@hotmail.com>
2025-01-15 22:01:06 +0100

    immagini, lista e profilo

---
+++ src/PHP/header.php
@@ -0,0 +1,13 @@
+<?php
+if(!isset($_SESSION)){
+    session_start();
+    
+}
+$DOM = file_get_contents('../HTML/header.html');
+if (!empty($_SESSION['username'])) {
+    $DOM = str_replace('<li class="navbar-item"><a href="accedi.php">Accedi</a></li>', '<li class="navbar-item"><a href="profilo.php">Il mio profilo</a></li>' , $DOM);
+    $DOM = str_replace('<li class="navbar-item"><a href="registrati.php">Registrati</a></li>', "", $DOM);
+}
+
+echo ($DOM);
+?>
