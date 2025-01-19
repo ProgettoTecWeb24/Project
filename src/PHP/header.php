454e15be7e82e678b752b86b5470d7a72dfca7cd
Blueerr <salvogiova01@hotmail.com>
2025-01-19 16:43:38 +0100

    fix header

--- src/PHP/header.php
+++ src/PHP/header.php
@@ -8,7 +8,7 @@ if (!empty($_SESSION['username'])) {
     $DOM = str_replace('<li class="navbar-item"><a href="accedi.php">Accedi</a></li>', '<li class="navbar-item"><a href="profilo.php">Il mio profilo</a></li>' , $DOM);
     $DOM = str_replace('<li class="navbar-item"><a href="registrati.php">Registrati</a></li>', "", $DOM);
 }
-$DOM = str_replace('<span lang="en">', '<span lang="en">' . $location, $DOM);
+$DOM = str_replace('<p>Ti trovi in: <span lang="en">', '<p>Ti trovi in: <span lang="en">' . $location, $DOM);
 
 echo ($DOM);
 ?>
