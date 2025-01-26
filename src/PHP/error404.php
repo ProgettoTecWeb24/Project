f6b6ea2cbe71e7fa7831db1f5844cf574ee2651a
MarinDavide <marin.davide03@gmail.com>
2025-01-26 18:06:41 +0100

    redirect pagine errore

--- src/PHP/error404.php
+++ src/PHP/error404.php
@@ -9,11 +9,6 @@ use Conn\DbConnection;
 setlocale(LC_ALL, 'it_IT');
 $connection = new DbConnection();
 
-//$title = "Page not found - Onoranze Stecca";
-//$page = "error404";
-//$description = "Pagina non trovata - CorsaIdeale";
-//$keywords = "404,error,page not found";
-
 include "header.php";
 
 $HTMLpage = file_get_contents('../HTML/error404.html');
