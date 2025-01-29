a4ebbc40931d443311b0e8adb085b38cd3e0cf57
Alfredo <alfredorubino1@gmail.com>
2025-01-29 23:26:21 +0100

    prova metatag
    caricati in header.html e index.php solo commentati

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+//$title = "CorsaIdeale - Home";
+//$description = "CorsaIdeale: scopri consigli e recensioni dettagliate sulle migliori scarpe da corsa. Trova il modello perfetto grazie all'aiuto dei nostri esperti."; // 148 caratteri
+//$keywords = "corsa,scarpe,recensioni,running,trail,jogging,nike,adidas,asics"; // 63 caratteri
 require_once('dbconnection.php');
 session_start();
 
