676d768bf4c958481cb6a16368cf53698501f7b8
MarinDavide <marin.davide03@gmail.com>
2025-01-31 13:16:46 +0100

    Add search and filter functionality to shoe list in admin panel

--- adminModificaLista.php
+++ adminModificaLista.php
@@ -34,8 +34,48 @@ if($connection->isAdmin($_SESSION["username"])){
     }
 
     $lista_scarpe = "";
-    $all_shoes = $connection->getAllShoes();
+    //$all_shoes = $connection->getAllShoes();
 
+    $query = "SELECT * FROM SCARPA WHERE nome LIKE '%' ";
+
+
+    if(!empty($_POST['nomescarpa'])){
+        $query = "SELECT * FROM SCARPA WHERE nome LIKE '%" . $_POST['nomescarpa'] . "%' ";
+        $HTMLpage = str_replace('value=""', 'value="' . $_POST['nomescarpa'] . '" selected', $HTMLpage);
+
+    }
+
+    if (!empty($_POST['marca']) AND $_POST['marca'] != 'all') {
+        $query = $query . "AND marca = '" . $_POST['marca']. "' ";
+        $HTMLpage = str_replace('value="' . $_POST['marca'] . '"', 'value="' . $_POST['marca'] . '" selected', $HTMLpage);
+
+    }
+
+    if (!empty($_POST['tipo']) AND $_POST['tipo'] != 'all') {
+        $query = $query . "AND tipo = '" . $_POST['tipo']. "' ";
+        $HTMLpage = str_replace('value="' . $_POST['tipo'] . '"', 'value="' . $_POST['tipo'] . '" selected', $HTMLpage);
+    }
+
+    if (!empty($_POST['ordina'])) {
+        $HTMLpage = str_replace('value="' . $_POST['ordina'] . '"', 'value="' . $_POST['ordina'] . '" selected', $HTMLpage);
+        if($_POST['ordina'] == "nomeCres"){
+            $query = $query . "ORDER BY nome ASC ";
+        }elseif($_POST['ordina'] == "nomeDesc"){
+            $query = $query . "ORDER BY nome DESC ";
+        }elseif($_POST['ordina'] == "votoCres"){
+            $query = $query . "ORDER BY votoexp ASC ";
+        }elseif($_POST['ordina'] == "votoDesc"){
+            $query = $query . "ORDER BY votoexp DESC ";
+        }elseif($_POST['ordina'] == "ordNonStand"){
+            $query = $query . "ORDER BY data_aggiunta ASC ";
+        }else{
+            $query = $query . "ORDER BY data_aggiunta DESC ";
+        }
+    }else{
+        $query = $query . "ORDER BY data_aggiunta DESC ";
+    }
+
+    $all_shoes = $connection->query($query);
     $lista_scarpe .= '
         <div class="table-wrapper-admin">
             <p id="sum">Tabella che contiene tutte le scarpe presenti nel sito, sono presenti nome, marca, descrizione, tipo, <span lang="en">feedback</span> dell\'esperto e foto per ogni scarpa</p>
