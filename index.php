3ca9cfa4650fd4755d7405147884a0d56f56809a
MarinDavide <marin.davide03@gmail.com>
2025-01-31 09:40:03 +0100

    FIXATO LIKE

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if(isset($_POST['likePress'])){
             $qLike = "DELETE FROM LIKES WHERE username ='" . $_SESSION['username'] . "'AND scarpa_id ='" . $_POST['likePress'] . "'";
             $result = $connection->query($qLike);
         }
+        header('Location: index.php#'. htmlspecialchars($_POST['likePress']));
     }else{
         header('Location: accedi.php');
         die();
@@ -54,7 +55,7 @@ if ($result) {
                         <p class="feedback">Feedback: ' . htmlspecialchars($row['feedback']) . '</p>
                         <p class="tipo">Tipo: ' . htmlspecialchars($row['tipo']) . '</p>
                         <p class="tipo">Voto esperto: ' . htmlspecialchars($row['votoexp']) . '</p>
-                        <form action="index.php#'. htmlspecialchars($row['id']).'" method="POST">
+                        <form action="'.$_SERVER['PHP_SELF'].'" method="POST">
                             <button class="like-button" type="submit" name="likePress" value="' . htmlspecialchars($row['id']) .'" aria-label="bottone like scarpa id ' . htmlspecialchars($row['id']) . '">{like}<path
                                             d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                                     </svg>
